eafe9d8dd5eeddebbfab54d33a8587bcdd24f11f
piotrnarecki <nareckipiotraleksander@gmail.com>
2021-04-08 16:44:45 +0200

    Initial commit

---
+++ src/main/kotlin/BankAccount.kt
@@ -0,0 +1,44 @@
+import java.util.*
+
+class BankAccount {
+    var id: String = ""
+    var balance: Double = 0.0
+
+    constructor() {
+        this.id = getUniqueId()
+        this.balance = 0.0
+    }
+
+    constructor(id: String, balance: Double) {
+        this.id = id
+        this.balance = balance
+    }
+
+    fun withdrawMoney(amount: Double) {
+        balance = balance - amount
+
+    }
+
+    fun depositMoney(amount: Double) {
+        balance = balance + amount
+
+    }
+
+    fun showBalance() {
+        println( "balance: $balance PLN")
+    }
+
+    fun isPositive(): Boolean {
+        if (balance > 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    private fun getUniqueId(): String {
+        var id = UUID.randomUUID().toString()
+        return id
+    }
+
+}
---
+++ src/main/kotlin/l5z2.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    var bankAccount1 = BankAccount()
+    bankAccount1.showBalance()
+    bankAccount1.depositMoney(100.0)
+    bankAccount1.showBalance()
+    bankAccount1.withdrawMoney(50.75)
+    bankAccount1.showBalance()
+
+
+}
